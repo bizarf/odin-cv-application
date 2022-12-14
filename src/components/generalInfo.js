b444241652f9919b729869d76abf3c2b01810815
bizarf <snesmaster40@googlemail.com>
2022-12-14 16:38:57 +0000

    Cleared out unused code

--- src/components/generalInfo.js
+++ src/components/generalInfo.js
@@ -4,40 +4,12 @@ import React, { Component } from "react";
 class GeneralInfo extends Component {
     constructor(props) {
         super(props);
-
-        // this.state = {
-        //     name: "",
-        //     email: "",
-        //     phone: "",
-        // };
-
-        // this.handleInputChange = this.handleInputChange.bind(this);
-        // this.saveInput = this.saveInput.bind(this);
     }
 
     handleInputChange(e) {
         const { name, value } = e.target;
         this.setState({ [name]: value });
     }
-
-    // saveInput() {
-    //     const { name, email, phone } = this.state;
-    //     localStorage.setItem("name", name);
-    //     localStorage.setItem("email", email);
-    //     localStorage.setItem("phone", phone);
-    // }
-
-    // componentDidMount() {
-    //     let name = localStorage.getItem("name");
-    //     let email = localStorage.getItem("email");
-    //     let phone = localStorage.getItem("phone");
-
-    //     if (name === null) name = "";
-    //     if (email === null) email = "";
-    //     if (phone === null) phone = "";
-
-    //     this.setState({ name, email, phone });
-    // }
 
     render() {
         return (
